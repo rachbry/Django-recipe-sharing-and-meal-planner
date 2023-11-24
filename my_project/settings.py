fb5913839fdc4bcf62f164af45a5768ea3a5eccc
Rachel Cutler <rach_cutler@live.co.uk>
2023-11-24 12:21:25 +0000

    connected elephantSQL database

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,17 +89,21 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://gitpod.io",
-    "https://*.herokuapp.com"
-    ]
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://gitpod.io",
+#     "https://*.herokuapp.com"
+#     ]
 
 
 
